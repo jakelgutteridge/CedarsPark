28129b9b9025a8ea8b565416694503378b6af793
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-21 12:17:22 +0100

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -35,10 +35,10 @@ const headerHTML = `
       </span>
     </center>
   </header>
-  
+  <br>
     <header>
     <center>
-    <span style="line-height: 1.2; background-image: url("/headerbgline.png"); background-repeat: no-repeat; background-size: 100% 100%;">
+    <span style="line-height: 0.5; background-image: url("/headerbgline.png"); background-repeat: no-repeat; background-size: 100% 100%;">
       <table cellpadding="0px" cellspacing="0px" style="width: 100%; max-width: 950px;">
         <tr>
           <td>
