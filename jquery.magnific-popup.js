853525bb7b884be5ba948748576c1036f38f24a5
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-23 23:59:09 +0100

    Update jquery.magnific-popup.js

--- jquery.magnific-popup.js
+++ jquery.magnific-popup.js
@@ -21,6 +21,19 @@ if (typeof define === 'function' && define.amd) {
  * 
  */
 
+	gallery: {
+  enabled: false, // set to true to enable gallery
+
+  preload: [0,2], // read about this option in next Lazy-loading section
+
+  navigateByImgClick: true,
+
+  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+  tPrev: 'Previous (Left arrow key)', // title for left button
+  tNext: 'Next (Right arrow key)', // title for right button
+  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+}
 
 /**
  * Private static constants
