7fb3a90459b886bc0b54641d01ccda0c486cf745
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-08-24 16:39:59 +0100

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -47,7 +47,7 @@ const footerHTML = `
       <table cellpadding="3px 0px 3px 0px" cellspacing="0px" style="width: 100%; max-width: 950px; padding-top: 10px;">
         <tr>
           <td>
-            <a href="https://goo.gl/maps/qGNjaJsKsYP7dyQz8" target="_blank" style="color: #ffffff;">Theobalds Lane, Cheshunt EN8 8RU</a>
+            <a href="https://goo.gl/maps/qGNjaJsKsYP7dyQz8" target="_blank" style="color: #ffffff;">Cedars Park, Theobalds Lane, Cheshunt EN8 8RU</a>
           </td>
           <td>
             <span style="float: right;">
