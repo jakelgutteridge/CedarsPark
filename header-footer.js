42194c1938e4e8295c7e4ab16c01600edbddfe5d
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2023-09-04 01:11:16 +0100

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -22,10 +22,10 @@ const headerHTML = `
                 </div>
                 <div class="dropdown-content">
                   <a href="/index">Home</a>
+                  <a href="/friends">Friends</a>
                   <a href="/photos">Photos</a>
                   <a href="/nature">Nature</a>
                   <a href="/history">History</a>
-                  <a href="/friends">Friends</a>
 <!--                  <a href="/calendar">Calendar</a> -->
                   <a href="/memorials">Memorials</a>
                   <a href="/contact">Contact</a>
